dd02444b100a0b35330c65cd186f652ce870d50b
Lars <lars@riok.ch>
2023-08-02 20:25:57 +0200

    ToHashSet should return HashSet (#9)

--- Meziantou.Polyfill.Editor/M;System.Linq.Enumerable.ToHashSet``1(System.Collections.Generic.IEnumerable{``0},System.Collections.Generic.IEqualityComparer{``0}).cs
+++ Meziantou.Polyfill.Editor/M;System.Linq.Enumerable.ToHashSet``1(System.Collections.Generic.IEnumerable{``0},System.Collections.Generic.IEqualityComparer{``0}).cs
@@ -2,6 +2,6 @@
 
 static partial class PolyfillExtensions
 {
-    public static IEnumerable<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource>? comparer)
+    public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource>? comparer)
         => new HashSet<TSource>(source, comparer);
 }
