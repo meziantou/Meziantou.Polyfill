5975bed437b73e5456e0371cb1014be9ff362706
Gérald Barré <git@meziantou.net>
2023-08-02 10:09:35 -0400

    Remove useless using

--- Meziantou.Polyfill/Members.g.cs
+++ Meziantou.Polyfill/Members.g.cs
@@ -1730,7 +1730,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 
 namespace System.Collections.Generic
 {
