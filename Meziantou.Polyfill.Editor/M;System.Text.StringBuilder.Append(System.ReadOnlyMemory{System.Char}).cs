2bfddd62f1ed5b7fb4a66771f61d71858ef1c01b
Gérald Barré <meziantou@users.noreply.github.com>
2023-09-05 16:39:24 -0700

    Add Task.WaitAsync and optimize StringBuilder.Append(ReadOnlySpan) (#25)

--- Meziantou.Polyfill.Editor/M;System.Text.StringBuilder.Append(System.ReadOnlyMemory{System.Char}).cs
+++ Meziantou.Polyfill.Editor/M;System.Text.StringBuilder.Append(System.ReadOnlyMemory{System.Char}).cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Text;
 
 static partial class PolyfillExtensions
 {
     public static StringBuilder Append(this StringBuilder target, ReadOnlyMemory<char> value)
     {
-        if (value.IsEmpty)
-            return target;
-
+        if (!value.IsEmpty)
+        {
+            unsafe
+            {
+                fixed (char* valueChars = &MemoryMarshal.GetReference(value.Span))
+                {
+                    _ = target.Append(valueChars, value.Length);
+                }
+            }
+        }
         return target.Append(value.ToArray());
     }
 }
--- Meziantou.Polyfill.Editor/M;System.Text.StringBuilder.Append(System.ReadOnlySpan{System.Char}).cs
+++ Meziantou.Polyfill.Editor/M;System.Text.StringBuilder.Append(System.ReadOnlySpan{System.Char}).cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Text;
 
 static partial class PolyfillExtensions
 {
     public static StringBuilder Append(this StringBuilder target, ReadOnlySpan<char> value)
     {
-        if (value.IsEmpty)
-            return target;
+        if (!value.IsEmpty)
+        {
+            unsafe
+            {
+                fixed (char* valueChars = &MemoryMarshal.GetReference(value))
+                {
+                    _ = target.Append(valueChars, value.Length);
+                }
+            }
+        }
 
-        return target.Append(value.ToArray());
+        return target;
     }
 }
