7581acb75ec43ca48deda5778d675fc5debb2ae0
Gérald Barré <meziantou@users.noreply.github.com>
2025-08-27 20:56:22 -0400

    Use sdk (#57)

--- Meziantou.Polyfill.Generator/Program.cs
+++ Meziantou.Polyfill.Generator/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+#pragma warning disable MA0011 // IFormatProvider is missing
+#pragma warning disable MA0047 // Declare types in namespaces
+#pragma warning disable MA0048 // File name must match type name
+using System.Reflection;
 using System.Text;
 using Meziantou.Polyfill.Generator;
 using Microsoft.CodeAnalysis.CSharp;
@@ -31,7 +34,7 @@ var polyfills = assembly.GetManifestResourceNames()
               {
                   SymbolKind.NamedType => PolyfillKind.Type,
                   SymbolKind.Method => PolyfillKind.Method,
-                  _ => throw new Exception($"Unknown symbol kind '{symbol.Kind}'"),
+                  _ => throw new InvalidOperationException($"Unknown symbol kind '{symbol.Kind}'"),
               },
               OutputPath = Path.GetFileNameWithoutExtension(item)
                                .Replace(';', '_')
@@ -266,7 +269,7 @@ async Task GenerateReadme()
     }
 
     var content = await File.ReadAllTextAsync(path);
-    var newContent = Regex.Replace(content, "(?<=<!-- begin_polyfills -->\\r?\\n).*(?=<!-- end_polyfills -->)", "\n" + sb.ToString() + "\n", RegexOptions.Singleline);
+    var newContent = Regex.Replace(content, "(?<=<!-- begin_polyfills -->\\r?\\n).*(?=<!-- end_polyfills -->)", "\n" + sb.ToString() + "\n", RegexOptions.Singleline, Timeout.InfiniteTimeSpan);
     await File.WriteAllTextAsync(path, newContent);
 }
 
@@ -283,7 +286,7 @@ static FullPath GetRootPath()
     {
         currentFolder = currentFolder.Parent;
         if (currentFolder.IsEmpty)
-            throw new Exception("Cannot find the path from " + FullPath.CurrentDirectory());
+            throw new InvalidOperationException("Cannot find the path from " + FullPath.CurrentDirectory());
 
         fullPath = FullPath.Combine(currentFolder, suffix);
     }
@@ -324,7 +327,7 @@ static Polyfill[] SortPolyfills(Polyfill[] items)
     }
 }
 
-sealed class Polyfill
+internal sealed class Polyfill
 {
     public required int Index { get; set; }
     public required ISymbol Symbol { get; set; }
@@ -354,7 +357,7 @@ sealed class Polyfill
     }
 }
 
-enum PolyfillKind
+internal enum PolyfillKind
 {
     Type,
     Method,
--- Meziantou.Polyfill.Tests/UnitTest1.cs
+++ Meziantou.Polyfill.Tests/UnitTest1.cs
@@ -1,10 +1,16 @@
+#pragma warning disable CA1307
+#pragma warning disable CA1849
+#pragma warning disable CA2000
+#pragma warning disable MA0001
+#pragma warning disable MA0002
+#pragma warning disable MA0021
+#pragma warning disable MA0074
 using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Net;
