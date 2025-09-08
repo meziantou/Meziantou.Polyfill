5977364a0cb32c78852e508104e0c389e20b191c
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-09-08 20:04:43 +1000

    Add `IEnumerable<KVP<K,V>>.ToDictionary<K,V>(IEqualityComparer<K>?)`

---
+++ Meziantou.Polyfill.Editor/M;System.Linq.Enumerable.ToDictionary``2(System.Collections.Generic.IEnumerable{System.Collections.Generic.KeyValuePair{``0,``1}}).cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+static partial class PolyfillExtensions
+{
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
+        where TKey : notnull
+        => source.ToDictionary(comparer: null);
+}
---
+++ Meziantou.Polyfill.Editor/M;System.Linq.Enumerable.ToDictionary``2(System.Collections.Generic.IEnumerable{System.Collections.Generic.KeyValuePair{``0,``1}},System.Collections.Generic.IEqualityComparer{``0}).cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+static partial class PolyfillExtensions
+{
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+        this IEnumerable<KeyValuePair<TKey, TValue>> source,
+        IEqualityComparer<TKey>? comparer)
+            where TKey : notnull
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(paramName: nameof(source));
+        }
+
+        return new(source, comparer);
+    }
+}
