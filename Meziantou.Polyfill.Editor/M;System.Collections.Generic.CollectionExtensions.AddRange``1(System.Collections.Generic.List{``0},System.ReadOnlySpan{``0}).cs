a77bbe3e01a0d7235e918e27e96cf143f2f31741
Paulo Morgado <470455+paulomorgado@users.noreply.github.com>
2025-12-29 16:33:10 +0000

    Changed to not use `extension` keyword

--- Meziantou.Polyfill.Editor/M;System.Collections.Generic.CollectionExtensions.AddRange``1(System.Collections.Generic.List{``0},System.ReadOnlySpan{``0}).cs
+++ Meziantou.Polyfill.Editor/M;System.Collections.Generic.CollectionExtensions.AddRange``1(System.Collections.Generic.List{``0},System.ReadOnlySpan{``0}).cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 
 static partial class PolyfillExtensions
 {
-    extension<T>(List<T> list)
+    /// <summary>Adds the elements of the specified span to the end of the <see cref="List{T}"/>.</summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to which the elements should be added.</param>
+    /// <param name="source">The span whose elements should be added to the end of the <see cref="List{T}"/>.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="list"/> is null.</exception>
+    public static void AddRange<T>(List<T> list, ReadOnlySpan<T> source)
     {
-        /// <summary>Adds the elements of the specified span to the end of the <see cref="List{T}"/>.</summary>
-        /// <typeparam name="T">The type of elements in the list.</typeparam>
-        /// <param name="list">The list to which the elements should be added.</param>
-        /// <param name="source">The span whose elements should be added to the end of the <see cref="List{T}"/>.</param>
-        /// <exception cref="ArgumentNullException">The <paramref name="list"/> is null.</exception>
-        public void AddRange(ReadOnlySpan<T> source)
-        {
-            ArgumentNullException.ThrowIfNull(list);
+        ArgumentNullException.ThrowIfNull(list);
 
-            if (!source.IsEmpty)
+        if (!source.IsEmpty)
+        {
+            if (list.Capacity < list.Count + source.Length)
             {
-                if (list.Capacity < list.Count + source.Length)
+                list.Capacity = list.Count + source.Length;
+                foreach (var item in source)
                 {
-                    list.Capacity = list.Count + source.Length;
-                    foreach (var item in source)
-                    {
-                        list.Add(item);
-                    }
+                    list.Add(item);
                 }
             }
         }
