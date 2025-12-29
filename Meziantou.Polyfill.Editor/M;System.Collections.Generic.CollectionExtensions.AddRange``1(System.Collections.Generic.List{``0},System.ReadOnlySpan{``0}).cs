f2dd70d9793762148c5d6efb2f8b9f3f789abf5a
Paulo Morgado <470455+paulomorgado@users.noreply.github.com>
2025-12-29 16:44:34 +0000

    Add List<T>.AddRange(ReadOnlySpan<T>) polyfill extension (#110)
    * Add List<T>.AddRange(ReadOnlySpan<T>) polyfill extension
    
    Adds an extension method to List<T> for adding elements from a ReadOnlySpan<T>, ensuring sufficient capacity and null checks. Bumps project version to 1.0.81.
    
    * Changed to not use `extension` keyword
    
    * Update Meziantou.Polyfill.Editor/M;System.Collections.Generic.CollectionExtensions.AddRange``1(System.Collections.Generic.List{``0},System.ReadOnlySpan{``0}).cs
    
    Co-authored-by: Gérald Barré <meziantou@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gérald Barré <meziantou@users.noreply.github.com>

---
+++ Meziantou.Polyfill.Editor/M;System.Collections.Generic.CollectionExtensions.AddRange``1(System.Collections.Generic.List{``0},System.ReadOnlySpan{``0}).cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+static partial class PolyfillExtensions
+{
+    /// <summary>Adds the elements of the specified span to the end of the <see cref="List{T}"/>.</summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to which the elements should be added.</param>
+    /// <param name="source">The span whose elements should be added to the end of the <see cref="List{T}"/>.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="list"/> is null.</exception>
+    public static void AddRange<T>(this List<T> list, ReadOnlySpan<T> source)
+    {
+        ArgumentNullException.ThrowIfNull(list);
+
+        if (!source.IsEmpty)
+        {
+            if (list.Capacity < list.Count + source.Length)
+            {
+                list.Capacity = list.Count + source.Length;
+                foreach (var item in source)
+                {
+                    list.Add(item);
+                }
+            }
+        }
+    }
+}
