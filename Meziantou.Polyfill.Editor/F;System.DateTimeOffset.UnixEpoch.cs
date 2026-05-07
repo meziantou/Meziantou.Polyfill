eb54d856a58d465ad2f74cdc43dde738659729d3
Langthjem <dennis@langthjem.dk>
2026-05-07 15:30:39 +0200

    Reduce System.DateTimeOffset.UnixEpoch allocations

--- Meziantou.Polyfill.Editor/F;System.DateTimeOffset.UnixEpoch.cs
+++ Meziantou.Polyfill.Editor/F;System.DateTimeOffset.UnixEpoch.cs
@@ -1,8 +1,15 @@
+using System;
+
 static partial class PolyfillExtensions
 {
-    extension(System.DateTimeOffset)
+    extension(DateTimeOffset)
     {
-        // 621355968000000000L = ticks for January 1, 1970, 00:00:00 UTC
-        public static System.DateTimeOffset UnixEpoch => new System.DateTimeOffset(621355968000000000L, System.TimeSpan.Zero);
+        public static DateTimeOffset UnixEpoch => DateTimeOffsetHelpers.UnixEpoch;
     }
+}
+
+file static class DateTimeOffsetHelpers
+{
+    // 621355968000000000L = ticks for January 1, 1970, 00:00:00 UTC
+    public static readonly DateTimeOffset UnixEpoch = new DateTimeOffset(621355968000000000L, TimeSpan.Zero);
 }
